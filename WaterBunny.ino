8d6e02672fb269762ba6a662983d0bfbce2d042a
Torsten Amshove <torsten@amshove.net>
2013-10-27 18:11:34 +0100

    Map Temp values to celsius values (*1000)

--- WaterBunny.ino
+++ WaterBunny.ino
@@ -148,7 +148,7 @@ void loop(){
       newLine += ",";
       newLine += accZ;
       newLine += ",";
-      newLine += accTemp;
+      newLine += map(accTemp,-2048,2047,-4000,8500);
       storage_write(newLine);
     }
   }else{
